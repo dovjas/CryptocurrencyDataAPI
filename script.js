0f123ec7111c8d72f86b43ac72b4f3a5f164f097
dovjas <dovydas.jaseliunas@gmail.com>
2022-07-01 13:45:17 +0300

    Function for Current page and Next page btn

--- script.js
+++ script.js
@@ -2,15 +2,22 @@ const baseUrl = 'https://api.coinranking.com/v2/coins';
 const proxyUrl ='https://cors-anywhere.herokuapp.com/';
 const mainUrl = `${proxyUrl}${baseUrl}`
 console.log(mainUrl)
+const dataBody =  document.querySelector('#data-tbody');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
 let cryptoData = []
 let resPerPage = 10;
 let currPage = 1;
 
-const dataBody =  document.querySelector('#data-tbody');
 
-
-async function renderData() {
+async function renderData(page = 1) {
     await getData()
+
+    if(page == 1){
+        prevBtn.getElementsByClassName.visibility = 'hidden'
+    } else {
+        prevBtn.style.visibility = 'visible'
+    }
 
     //Create HTML
 
@@ -22,12 +29,11 @@ async function renderData() {
         if(idx >= start && idx < end) return true;
     }).forEach(coin => {
         coinData += "<tr>";
-        coinData += `<td>${parseFloat(coin.btcPrice).toFixed(5)}</td>`;
         coinData += `<td>${(coin.rank)}</td>`;
-        coinData += `<td>${(coin.tier)}</td>`;
         coinData += `<td>${(coin.name)}</td>`;
-        coinData += `<td>${(coin.symbol)}</td>`;
         coinData += `<td>${Math.round(coin.price)}</td>`;
+        coinData += `<td>${parseFloat(coin.btcPrice).toFixed(5)}</td>`;
+        coinData += `<td>${(coin.symbol)}</td>`;
         "<tr>"
         console.log(coinData)
 
@@ -35,13 +41,35 @@ async function renderData() {
 
     dataBody.innerHTML = coinData;
 }
-renderData()
+renderData(currPage)
+
+// Next  page button function
+
+function nextPage(){
+    if((currPage * resPerPage) < cryptoData.length){
+        currPage++;
+        renderData()
+    }
+
+}
+// Previous  page button function
+
+function prevPage(){
+    if (currPage > 1){
+        currPage --;
+        renderData()
+    }
+}
+
+prevBtn.addEventListener('click', prevPage,false)
+nextBtn.addEventListener('click', nextPage,false)
+
 
 //Fetch Data 
 async function getData() {
     const response = await fetch(mainUrl);
     const crypto = await response.json();
-    cryptoData = crypto.data.coins 
+    cryptoData = crypto.data.coins
     console.log(cryptoData)
 }
 
